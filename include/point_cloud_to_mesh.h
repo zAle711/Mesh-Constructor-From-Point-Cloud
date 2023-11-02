ba340b4b75adaa075d447adb577e99665e76f30a
root <root@DESKTOP-LAE9LAK>
2023-11-02 14:53:35 +0100

    Clean code

--- include/point_cloud_to_mesh.h
+++ include/point_cloud_to_mesh.h
@@ -1,6 +1,8 @@
 #ifndef POINTCLOUDTOMESH_H
 #define POINTCLOUDTOMESH_H
 
+#include <string> 
+
 #include <pcl/point_cloud.h>
 #include <pcl/PCLPointCloud2.h>
 #include <pcl/filters/filter.h>
@@ -13,6 +15,7 @@
 #include <pcl/surface/mls.h>
 #include <pcl/filters/radius_outlier_removal.h>
 #include <pcl/surface/gp3.h>
+#include <pcl/io/pcd_io.h>
 
 //template <typename PointT, typename PointTNormal>
 class PointCloudToMesh
@@ -24,78 +27,43 @@ class PointCloudToMesh
 	
 	void set_input(const boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> point_cloud_input)
 	{
-		point_cloud = point_cloud_input;
+		new_point_cloud = point_cloud_input;
 	}
+	
+	
 	
 	bool compute_mesh()
 	{
-		if (!point_cloud.get()) return false;
 		
-		if (point_cloud->size() <= 0) return false;
+		if (new_point_cloud->size() <= 0) return false;
 		
-		std::cout << "Dimensione Point Cloud: "<< point_cloud->size() << std::endl;
+		if (!FilterPointCloud()) {
+			ROS_WARN("Point Cloud Downsampling Failed!");
+			return false;
+		}
 		
-		//RIMUOVO VALORI NAN
-				
-		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_no_nan  (new pcl::PointCloud<pcl::PointXYZ>);
-		std::vector<int> indices;
-		pcl::removeNaNFromPointCloud(*point_cloud, *cloud_no_nan, indices);
-		
-		std::cout << "Dimensione Point Cloud senza valori NaN: " << cloud_no_nan->size() << std::endl;
-		
-		/*
-		//RIMUOVO OUTLIER
-		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_no_outlier (new pcl::PointCloud<pcl::PointXYZ>);
-		
-		pcl::RadiusOutlierRemoval<pcl::PointXYZ> outrem;
-		outrem.setInputCloud(cloud_no_nan);
-    outrem.setRadiusSearch(0.8);
-    outrem.setMinNeighborsInRadius (2);
- 		outrem.setKeepOrganized(true);
-    // apply filter
-    outrem.filter (*cloud_no_outlier);
-		*/
-		//APPLICO VOXELGRID PER FARE UN DOWNSAMPLING
-		
-	  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_voxelized (new pcl::PointCloud<pcl::PointXYZ>);
-		pcl::VoxelGrid<pcl::PointXYZ> gridFilter;
-		gridFilter.setInputCloud (cloud_no_nan);
-		gridFilter.setLeafSize (0.05f, 0.05f, 0.05f);
-		gridFilter.filter (*cloud_voxelized);
-		filtered_point_cloud = *cloud_voxelized;
-		std::cout << "Numero di punti dopo aver applicato il filtro " << cloud_voxelized->size() << std::endl;
+		new_point_cloud.reset(new pcl::PointCloud<pcl::PointXYZ>);
 		
 		//STIMO LA NORMALE PER OGNI PUNTO
 		pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
 		
 		pcl::search::KdTree<pcl::PointXYZ>::Ptr tree_ne (new pcl::search::KdTree<pcl::PointXYZ>);
-		tree_ne->setInputCloud (cloud_voxelized);
+		tree_ne->setInputCloud (filtered_point_cloud);
 		
 		pcl::NormalEstimationOMP<pcl::PointXYZ, pcl::Normal> ne;
-		ne.setInputCloud (cloud_voxelized);
+		ne.setInputCloud (filtered_point_cloud);
 		ne.setSearchMethod (tree_ne);
 		ne.setKSearch (20);
 		ne.compute (*normals);
-		std::cout << "Normali stimate" << std::endl;
+		//std::cout << "Normali stimate" << std::endl;
 		
 		//UNISCO LA POINT CLOUD CON LE NORMALI CALCOLATE
 	  pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointNormal>);
-  	pcl::concatenateFields(*cloud_voxelized, *normals, *cloud_with_normals);
+  	pcl::concatenateFields(*filtered_point_cloud, *normals, *cloud_with_normals);
   	
   	pcl::search::KdTree<pcl::PointNormal>::Ptr tree (new pcl::search::KdTree<pcl::PointNormal>);
 		tree->setInputCloud (cloud_with_normals);
-		/*
-		//COSTRUISCO LA MESH (MARCHING CUBE) -- NON E' MOLTO BELLO
-		pcl::MarchingCubesHoppe<pcl::PointNormal> mc;
-		mc.setIsoLevel (0);
-		mc.setGridResolution (30,30,30);
-		mc.setPercentageExtendGrid (0.3f);
-		//pcl::MarchingCubesRBF<pcl::PointNormal> mc;
-		//pcl::PolygonMesh::Ptr triangles(new pcl::PolygonMesh);
-		mc.setInputCloud (cloud_with_normals);
-		mc.setSearchMethod (tree);
-		mc.reconstruct (mesh);
-		*/
+		
 		//COSTRUISCO LA MESH GREEDPROYECTION
 		pcl::GreedyProjectionTriangulation<pcl::PointNormal> gp3;
 		
@@ -111,24 +79,70 @@ class PointCloudToMesh
 	  gp3.setSearchMethod (tree);
 	  gp3.reconstruct (mesh);
 		
+		return true;
+	}
+	
+	bool FilterPointCloud()
+	{
+		if (!new_point_cloud.get()) return false;
+		
+		if (new_point_cloud->size() <= 0) return false;
+		
+		//Rimuovo valori NaN
+		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_no_nan  (new pcl::PointCloud<pcl::PointXYZ>);
+		std::vector<int> indices;
+		pcl::removeNaNFromPointCloud(*new_point_cloud, *cloud_no_nan, indices);
+		
+		//VoxelGrid DownSampling
+	  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_voxelized (new pcl::PointCloud<pcl::PointXYZ>);
+		pcl::VoxelGrid<pcl::PointXYZ> gridFilter;
+		gridFilter.setInputCloud (cloud_no_nan);
+		gridFilter.setLeafSize (0.05f, 0.05f, 0.05f);
+		gridFilter.filter (*cloud_voxelized);
+		filtered_point_cloud = cloud_voxelized;
+		
+		if (point_cloud.size() == 0)
+		{
+			point_cloud = *filtered_point_cloud;
+		} 
+		else
+		{
+			
+			point_cloud += *filtered_point_cloud;
+		}
+	
+		ROS_INFO("Point Cloud original size: %lu, after removing NaN: %lu, after filtering: %lu", new_point_cloud->size(), cloud_no_nan->size(), cloud_voxelized->size());
+		
+		pcl::io::savePCDFileASCII ("/home/all_points.pcd", point_cloud);
 		
 		return true;
 	}
+	
+	bool UpdatePointCloud()
+	{
+		return false;
+	}
+	
 	
 	pcl::PolygonMesh getMesh()
 	{
 		return mesh;
 	}	
 	
-	pcl::PointCloud<pcl::PointXYZ> getFilteredPointCloud()
+	pcl::PointCloud<pcl::PointXYZ>::Ptr getFilteredPointCloud()
 	{
 		return 	filtered_point_cloud;
 	}
+	
+	pcl::PointCloud<pcl::PointXYZ> getGlobalPointCloud()
+	{
+		return point_cloud;
+	}
 	private:
 	
-	boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> point_cloud;
-	
-	pcl::PointCloud<pcl::PointXYZ> filtered_point_cloud;
+	pcl::PointCloud<pcl::PointXYZ> point_cloud;
+	boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> new_point_cloud;
+	boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> filtered_point_cloud;
 	
 	pcl::PolygonMesh mesh;
 
--- include/point_cloud_to_mesh_ros.h
+++ include/point_cloud_to_mesh_ros.h
@@ -7,10 +7,12 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <shape_msgs/Mesh.h>
+#include <tf/transform_listener.h>
 //PCL 
 #include <pcl/point_cloud.h>
 #include <pcl/PCLPointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/transforms.h>
 
 
 //template <typename PointT>
@@ -26,42 +28,75 @@ public:
 		shape_pub = nh.advertise<shape_msgs::Mesh>("mesh_shape", 1, true);
 		f_cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("filtered_point_cloud",1, true);
 		
+		
+		
 	}
 		
 	void cloudCallBack(const sensor_msgs::PointCloud2::ConstPtr& cloud_in)
 	{
+		ROS_INFO("Messaggio Arrivato!");
+		
+		sensor_msgs::PointCloud2 cloud_out;
+		tf::StampedTransform transform;
+		
+		bool transform_done = pcl_ros::transformPointCloud("world", *cloud_in, cloud_out, listener);
+		if (!transform_done) 
+		{
+			listener.waitForTransform("world", "openni_rgb_optical_frame", ros::Time(0), ros::Duration(0.5));
+			if (pcl_ros::transformPointCloud("world", *cloud_in, cloud_out, listener)) 
+			{
+				ROS_INFO("Message Discarded. Can't transform from /openni_rgb_optical_frame --> /world ");
+				return;
+			}
+		}
+		
+		/*
+		while (true) {
+			
+			if (pcl_ros::transformPointCloud("/world", *cloud_in, cloud_out, listener)) break;
+			std::cout << "Aspetto la trasformata per 0.5 secondi" << std::endl;
+			listener.waitForTransform("/world", "/openni_rgb_optical_frame", ros::Time(0), ros::Duration(0.5));
+		}
+		
+		while (!pcl_ros::transformPointCloud("/world", *cloud_in, cloud_out, listener)) {
+			std::cout << "dentro la while per la trasformata!" << std::endl;
+		}
+		
+				 
+		//listener.lookupTransform(cloud->header.frame_id, "world", 
+		//std::cout << cloud_in->header.frame_id << std::endl;
+		*/
 		boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> point_cloud (new pcl::PointCloud<pcl::PointXYZ>());
-		pcl::fromROSMsg(*cloud_in, *point_cloud);
+		pcl::fromROSMsg(cloud_out, *point_cloud);
 		
 		cloud_to_mesh.set_input(point_cloud);
 		
 		if (cloud_to_mesh.compute_mesh())
 		{
-		
+			
 			if (shape_pub.getNumSubscribers() > 0)
 			{
 				shape_msgs::Mesh mesh_msg;
 				
 				meshToShapeMsg(cloud_to_mesh.getMesh(), mesh_msg);
 				shape_pub.publish(mesh_msg);
-			
-			
+				ROS_INFO("Pubblicata Mesh Ricostruita");
 			
 			}
 			
-			if (f_cloud_pub.getNumSubscribers() > 0)
+		
+
+		
+			if (f_cloud_pub.getNumSubscribers() > 0 )
 			{
-			
 				sensor_msgs::PointCloud2 point_cloud_msg;
-				pcl::toROSMsg(cloud_to_mesh.getFilteredPointCloud(), point_cloud_msg);
+				pcl::toROSMsg(cloud_to_mesh.getGlobalPointCloud(), point_cloud_msg);
 				f_cloud_pub.publish(point_cloud_msg);
-			
+				ROS_INFO("Pubblicata Point Cloud Filtrata");
 			
 			}
 		
 		}
-		
-		ROS_INFO("Point Cloud ricevuta!");
 	}
 	
 
@@ -69,6 +104,7 @@ private:
 	ros::Subscriber cloud_sub;
 	ros::Publisher shape_pub;
 	ros::Publisher f_cloud_pub;
+	tf::TransformListener listener;
 	
 	PointCloudToMesh cloud_to_mesh;
 	
